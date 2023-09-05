327073d20826c1b1c9cc5f1a33616241ca1efb67
SahinS9 <shahinsaatov@gmail.com>
2023-09-05 17:43:20 +0400

    short data cleaning on the sqlite3 table

---
+++ data_cleaning.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sqlite3
+import re
+
+
+#Connect to the Database - If it exists
+conn = sqlite3.connect('products.db')
+c = conn.cursor()
+
+# Query All needed data
+df = c.execute("Select * from products").fetchall()
+
+#Create DataFrame with proper column names
+df = pd.DataFrame(df, columns = ['code','pr','price_azn','price_exc_vat_azn'])
+
+#Take numerical data out of the columns with regex
+df['price_azn'] = df.price_azn.apply(lambda x : re.findall(r'\d+\.\d+', x)[0])
+df['price_exc_vat_azn'] = df.price_exc_vat_azn.apply(lambda x : re.findall(r'\d+\.\d+', x)[0])
+
+#Set column type as Float in order to be able to work on it
+df.price_azn = df.price_azn.astype('float64')
+df.price_exc_vat_azn = df.price_exc_vat_azn.astype('float64')
+
+#Have output in order to report in Excel
+df.to_excel('output.xlsx', sheet_name= "web_scraping", index = False)
+
--- scrapy.py
+++ scrapy.py
@@ -2,18 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-baseurl = "https://www.thewhiskyexchange.com/"
 
+#user agent to get data
 headers = {
     'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 }
 
-# x = 1
-
-# r = requests.get(f'https://www.compusale.az/index.php?route=product/manufacturer/info&manufacturer_id=2&page={x}')
-
+#empty list in order to add dictionaries in it during scraping
 p = []
 
+#loop thru pages of the website within defined range
 for x in range(1,21):
     r = requests.get(f'https://www.compusale.az/index.php?route=product/manufacturer/info&manufacturer_id=2&page={x}')
     soup = BeautifulSoup(r.content,  features = 'lxml')
@@ -23,6 +21,7 @@ for x in range(1,21):
 
 
 
+#Take needed data from each object to the dictionary and add it to the List
     for x in productlist:
         
        p.append( {'product_code':x.find('span', class_= 'stat-2').find_all('span')[1].string,
@@ -33,11 +32,13 @@ for x in range(1,21):
 
 #print(p)
 
-import sqlite3
-
+#Connect to the DB OR Create if it does not exist
 conn = sqlite3.connect('products.db')
 c = conn.cursor()
 
+
+
+#Create table If not exists
 c.execute("""
     CREATE Table IF NOT EXISTS products (
           product_code TEXT,
@@ -48,9 +49,8 @@ c.execute("""
 
 """)
 
-product_list = []
 
-
+#INSERT INTO Table one by one in the loop / *BULK INSERT IS BETTER OPTION
 def insert_pr(u):
     with conn:
         # print(u)
@@ -58,7 +58,8 @@ def insert_pr(u):
         c.execute("INSERT INTO products (product_code, product_name, price_inc_vat, price_exc_vat) VALUES (?, ?, ?, ?)",
               (u['product_code'], u['product_name'], u['price_inc_vat'], u['price_exc_vat']) )
         
-#insert_pr(p[0])
+
+#call function to insert and start the process in order to use this data in data_cleaning.py to get needed values
 for pl in p:
     print(pl)
     print(pl['product_name'])
